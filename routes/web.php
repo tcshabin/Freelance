f692385bcef200552f9fba2f6b9e25c8cea60401
SHABIN7009 <shabintc@bpract.com>
2023-01-11 20:01:36 +0530

    lets-check

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,6 @@ Route::get('/', function () {
     return redirect('/register');
 });
 
-Route::get('/tasks', 'App\Http\Controllers\TaskController@Index');
-Route::get('/tasks_data', 'App\Http\Controllers\TaskController@Data');
-Route::get('/create', 'App\Http\Controllers\TaskController@Create');
-Route::post('/store', 'App\Http\Controllers\TaskController@Store');
-Route::get('/tasks/{id}', 'App\Http\Controllers\TaskController@Show');
-Route::put('/update-task/{id}', 'App\Http\Controllers\TaskController@Update');
-Route::delete('/task/{id}', 'App\Http\Controllers\TaskController@Destroy')->name('task.destroy');
-Route::get('priority_update', 'App\Http\Controllers\TaskController@PriorityUpdate');
-
 Route::get('login','App\Http\Controllers\Auth\LoginController@Login')->name('login');
 Route::get('logout', 'App\Http\Controllers\Auth\LoginController@Logout')->name('logout');
 // insta start
